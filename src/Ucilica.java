4f758b52090971f3aea7b1dbd1cb36240058b89d
BottleUser222 <marko.s.mirkovic@gmail.com>
2026-02-26 18:55:23 +0100

    Create Ucilica class for an ArrayList of GeometrijskiLik.

---
+++ src/Ucilica.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Ucilica {
+
+    ArrayList<GeometrijskiLik> geolikovi = new ArrayList<>();
+
+    public Ucilica() {
+        this.geolikovi = geolikovi;
+    }
+    public ArrayList<GeometrijskiLik> getGeolikovi() {
+        return geolikovi;
+    }
+}
